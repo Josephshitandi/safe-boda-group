8cf65c41d93a766d0c234050019c3d8e22a9b9f6
sophieO1970 <sophieO1970@gmail.com>
2020-11-03 13:48:55 +0300

    create a decoration for the password and a function password to check for the rider's password and verify it

--- app/models.py
+++ app/models.py
@@ -17,3 +17,19 @@ class Rider(UserMixin,db.Model):
     pass_secure = db.Column(db.String(255))
     number_plate = db.Column(db.String(255),unique = True,index = True)
     motorbike_model = db.Column(db.String(255))
+    
+
+ @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
+    def __repr__(self):
+        return f'Rider {self.ridername}'
