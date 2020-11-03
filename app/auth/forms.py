d02b784162916a6466d9727b9c681b73e16cfd42
sophieO1970 <sophieO1970@gmail.com>
2020-11-03 13:56:24 +0300

    create a form for the class rider with its fields

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,3 +3,21 @@ from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import Required,Email,EqualTo,ValidationError
 from ..models import Rider
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
+
+
+class RiderForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    ridername = StringField('Enter your ridername',validators = [Required()])
+    number_plate StringField('Your motor number plate',validators =[Required(),number plate())]
+    Motor_model StringField('Your motor model type',validators =[Required(),motor model())]
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Upload')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email =data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_ridername(self,data_field):
+        if User.query.filter_by(ridername = data_field.data).first():
+            raise ValidationError('That ridername is taken')
