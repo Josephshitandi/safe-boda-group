56090dd6ed74f7711343ea4b8e9e471c193200fd
Fred <akamaatei@gmail.com>
2020-11-04 10:19:48 +0300

    fix index file

--- app/auth/views.py
+++ app/auth/views.py
@@ -15,7 +15,7 @@ def register():
         db.session.commit()
 
         return redirect(url_for('auth.login'))
-    title = "Pomodoro"
+    title = "Safe-Boda"
     return render_template('auth/register.html',registration_form = form, title = title)
 
 @auth.route('/login',methods=['GET','POST'])
@@ -29,7 +29,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "Pomodoro login"
+    title = "Safe-Boda login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/logout')
