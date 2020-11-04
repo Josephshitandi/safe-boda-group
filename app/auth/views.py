9b4acf0e9ac76d9e1b6d2135d8354f9a27ab661b
sophieO1970 <sophieO1970@gmail.com>
2020-11-04 09:06:12 +0300

    create routes for sugn up,logout and login

---
+++ app/auth/views.py
@@ -0,0 +1,36 @@
+from flask import render_template,url_for,flash,redirect,request
+from . import auth
+from flask_login import login_user,login_required,logout_user
+from .forms import RegistrationForm,LoginForm
+from ..models import Rider
+from .. import db
+from ..email import mail_message
+
+@auth.route('/login', methods = ['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        rider = Rider.query.filter_by(ridername = form.ridername.data).first()
+        if rider != None and rider.verify_password(form.password.data):
+            login_rider(rider,form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid ridername or Password')
+    return render_template('auth/login.html', loginform = form)
+
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
+
+@auth.route('/signup', methods = ["GET","POST"])
+def signup():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        rider = Rider(email = form.email.data, ridername = form.ridername.data, password = form.password.data)
+        rider.save_rider()
+        mail_message("Welcome to Safe-Boda","email/welcome_rider",rider.email,rider=rider)
+        return redirect(url_for('auth.login'))
+    return render_template('auth/signup.html', r_form = form)
