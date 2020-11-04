17b33944b1066901ca345d7f7c0cd9a37030def7
DennisKipkirui <kipkiruimutai33@gmail.com>
2020-11-04 18:53:48 +0300

    restructure folders

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,17 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask import Blueprint
 main = Blueprint('main',__name__)
-from . import views,error
+# from . import views,
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+login_manager = LoginManager()
+
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -10,6 +21,7 @@ def create_app(config_name):
 
     # Initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
