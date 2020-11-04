51e17f3ea96f83aac1998160ca5eeec559b40914
sophieO1970 <sophieO1970@gmail.com>
2020-11-04 08:56:52 +0300

    add the secret key

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY ='3005c01f560af999a1c88e59d059ce'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:sophiecee@localhost/safe-boda'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
@@ -11,8 +11,8 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME ='sophieolondie@gmail.com'
+    MAIL_PASSWORD ='whwtyfvhukzsfcgv'
     
     # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
