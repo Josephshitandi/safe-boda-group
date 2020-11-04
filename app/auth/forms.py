621edb374fea5926e4664519d9ee72bf003119de
sophieO1970 <sophieO1970@gmail.com>
2020-11-04 09:04:50 +0300

    add registration form and login form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,20 +4,26 @@ from wtforms.validators import Required,Email,EqualTo,ValidationError
 from ..models import Rider
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
 
-
-class RiderForm(FlaskForm):
+class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
     ridername = StringField('Enter your ridername',validators = [Required()])
-    number_plate StringField('Your motor number plate',validators =[Required(),number plate())]
-    Motor_model StringField('Your motor model type',validators =[Required(),motor model())]
     password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
-    submit = SubmitField('Upload')
+    submit = SubmitField('Sign Up')
 
     def validate_email(self,data_field):
-        if User.query.filter_by(email =data_field.data).first():
-            raise ValidationError('There is an account with that email')
+        if Rider.query.filter_by(email =data_field.data).first():
+            raise ValidationError('Sorry! There is an account with that email')
 
     def validate_ridername(self,data_field):
-        if User.query.filter_by(ridername = data_field.data).first():
-            raise ValidationError('That ridername is taken')
+        if Rider.query.filter_by(ridername = data_field.data).first():
+            raise ValidationError('Sorry!That ridername is already taken')
+
+
+        
+class LoginForm(FlaskForm):
+    ridername = StringField('Ridername',validators=[Required()])
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
